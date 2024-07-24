cd71f1c6ec0febf9673965dede1171481de02d62
Dan <yegenius95@gmail.com>
2024-07-24 11:18:18 +0900

    마이페이지 내 이용 상태 박스 하단에 메뉴아이템 테이블 생성 * UITableViewDelegate, UITableVIewDataSource * UI 요소 내 이용 상태에 따른 조건문 삭제 후 updateUI() 생성 * timeTitleLabel 생성 : "이용시간" 크기 조정 * 요소 간 간격 조정

--- kickboardBookingAppProject/kickboardBookingAppProject/myPageVC.swift
+++ kickboardBookingAppProject/kickboardBookingAppProject/myPageVC.swift
@@ -31,7 +31,7 @@ struct UserInfo {
 // D. 더미 데이터
 var user = UserInfo(userID: "johnDoe123", password: "securePassword", nickname: "John", kickboardCode: "KFSE123", isUsingScooter: true)
 
-class myPageVC: UIViewController {
+class myPageVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     // UI 요소 정의 : greetingLabel
     let greetingLabel: UILabel = {
@@ -40,12 +40,6 @@ class myPageVC: UIViewController {
         label.textAlignment = .left
         label.font = .boldSystemFont(ofSize: 24)
         label.translatesAutoresizingMaskIntoConstraints = false
-        // D. 이용 여부에 따른 라벨 텍스트 변화를 주는 조건문
-        if user.isUsingScooter {
-            label.text = "\(user.nickname)님\n헬멧을 꼭 써주세요!"
-        } else {
-            label.text = "\(user.nickname)님\n오늘도 달려볼까요?"
-        }
         return label
     }()
     
@@ -64,19 +58,33 @@ class myPageVC: UIViewController {
     let statusLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        // 이용 시간: 만약 이용중이 아닐 경우 빈 공간
-        label.text = "\(user.kickboardCode)호 이용중"
         label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        return label
+    }()
+    
+    let centerLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        label.textAlignment = .center
+        label.isHidden = true
+        return label
+    }()
+    
+    let timeTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        label.numberOfLines = 0
+        label.textAlignment = .right
         return label
     }()
     
     let timeLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        // 이용시간: 만약 이용중이 아닐 경우 빈 공간 * 이용 등록 시간이 필요함(해라님과 협업)
-        label.text = "이용시간\n2024.07.22. 18:00 26분째"
         label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
-        label.numberOfLines = 2
+        label.numberOfLines = 0
         label.textAlignment = .right
         return label
     }()
@@ -84,13 +92,21 @@ class myPageVC: UIViewController {
     lazy var returnButton: UIButton = {
         let button = UIButton(type: .system)
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("반납 하기", for: .normal)
         button.backgroundColor = .red
         button.setTitleColor(.white, for: .normal)
         button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
         button.addTarget(self, action: #selector(returnButtonTapped), for: .touchUpInside)
         return button
     }()
+    
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    // 테이블 뷰 메뉴 아이템
+    let menuItems = ["이용 내역", "회원정보 변경", "운전면허증(업데이트 예정)", "로그아웃"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -103,10 +119,16 @@ class myPageVC: UIViewController {
         
         view.addSubview(containerView)
         containerView.addSubview(statusLabel)
+        containerView.addSubview(centerLabel)
+        containerView.addSubview(timeTitleLabel)
         containerView.addSubview(timeLabel)
         containerView.addSubview(returnButton)
         
+        view.addSubview(tableView)
+        
         setUpLayout()
+        updateUI()
+        setUpTableView()
     }
     
     func setUpLayout() {
@@ -119,7 +141,7 @@ class myPageVC: UIViewController {
         
         // 컨테이너 뷰 제약조건
         containerView.snp.makeConstraints {
-            $0.top.equalTo(greetingLabel.snp.bottom).offset(20)
+            $0.top.equalTo(greetingLabel.snp.bottom).offset(30)
             $0.leading.equalToSuperview().offset(24)
             $0.trailing.equalToSuperview().offset(-24)
             $0.height.equalTo(150)
@@ -131,10 +153,22 @@ class myPageVC: UIViewController {
             $0.leading.equalTo(containerView.snp.leading).offset(16)
         }
         
-        // 이용 시간 레이블 제약 조건
-        timeLabel.snp.makeConstraints {
+        // 이용 시간 타이틀 레이블 제약 조건
+        timeTitleLabel.snp.makeConstraints {
             $0.top.equalTo(statusLabel.snp.bottom).offset(10)
             $0.trailing.equalTo(containerView.snp.trailing).offset(-16)
+        }
+        
+        // 이용 시간 레이블 제약 조건
+        timeLabel.snp.makeConstraints {
+            $0.top.equalTo(timeTitleLabel.snp.bottom).offset(10)
+            $0.trailing.equalTo(containerView.snp.trailing).offset(-16)
+        }
+        
+        // 중앙 라벨 제약 조건
+        centerLabel.snp.makeConstraints {
+            $0.top.equalTo(containerView).offset(50)
+            $0.centerX.equalTo(containerView)
         }
         
         // 반납 버튼 레이블 제약 조건
@@ -145,6 +179,60 @@ class myPageVC: UIViewController {
             $0.height.equalTo(50)
         }
         
+        // 테이블 뷰 레이블 제약 조건
+        tableView.snp.makeConstraints {
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.top.equalTo(containerView.snp.bottom).offset(40)
+            $0.bottom.equalToSuperview()
+        }
+        
+    }
+    
+    // 이용 여부에 따른 라벨 및 버튼 텍스트 업데이트하는 메서드
+    func updateUI() {
+        
+        if user.isUsingScooter {
+            greetingLabel.text = "\(user.nickname)님\n헬멧을 꼭 써주세요!"
+            statusLabel.text = "\(user.kickboardCode)호 이용중"
+            timeTitleLabel.text = "이용시간"
+            timeLabel.text = "2024.07.22. 18:00 26분째"
+            centerLabel.text = ""
+            returnButton.setTitle("반납 하기", for: .normal)
+            centerLabel.isHidden = true
+            statusLabel.isHidden = false
+            timeTitleLabel.isHidden = false
+            timeLabel.isHidden = false
+        } else {
+            greetingLabel.text = "\(user.nickname)님\n오늘도 달려볼까요?"
+            statusLabel.text = ""
+            timeTitleLabel.text = ""
+            timeLabel.text = ""
+            centerLabel.text = "이용중인 킥보드가 없어요."
+            returnButton.setTitle("이용하러 가기", for: .normal)
+            centerLabel.isHidden = false
+            statusLabel.isHidden = true
+            timeTitleLabel.isHidden = true
+            timeLabel.isHidden = true
+        }
+    }
+    
+    // 이용내역 등 안내 테이블 뷰
+    func setUpTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        menuItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = menuItems[indexPath.row]
+        return cell
     }
     
     @objc func returnButtonTapped() {
