2eea702cdd6e33ae3c1acc481aa7ae95f606bfe4
Dan <yegenius95@gmail.com>
2024-07-28 18:09:40 +0900

    마이페이지애서 로그아웃 탭 시 로그인/회원가입 페이지로 전환

--- kickboardBookingAppProject/kickboardBookingAppProject/AppDelegate.swift
+++ kickboardBookingAppProject/kickboardBookingAppProject/AppDelegate.swift
@@ -12,8 +12,6 @@ import KakaoMapsSDK
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         SDKInitializer.InitSDK(appKey: "fee1b2fd7185bc9d8c9c433b337417a7")
         
